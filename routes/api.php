aa5932adddfdc9a9c068c3799ec26d9a7648c5a7
Sohail Afzal <sohail.afzal@barqaab.com>
2025-04-02 17:24:45 +0500

    02-04-2025

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,11 @@ Route::get('/gepco/test', function () {
 
 Route::post('/gepco/login', [LoginController::class, 'login']);
 
-Route::middleware(['auth:sanctum'])->get('/gepco/options', [GepcoController::class, 'options']);
-Route::middleware(['auth:sanctum'])->post('/gepco/earthingDetail', [GepcoController::class, 'earthingDetail']);
-Route::post('/gepco/save', [GepcoController::class, 'saveEarthingDetail']);
-Route::get('/divisions/{cirle_id}', [GepcoController::class, 'getDivisions']);
-Route::get('/subdivisions/{division_id}', [GepcoController::class, 'getSubDivisions']);
-Route::get('/feeders/{sub_division_id}', [GepcoController::class, 'getfeeders']);
+Route::middleware(['auth:sanctum'])->prefix('gepco')->group(function () {
+    Route::get('/options', [GepcoController::class, 'options']);
+    Route::post('/earthingDetail', [GepcoController::class, 'earthingDetail']);
+    Route::post('/save', [GepcoController::class, 'saveEarthingDetail']);
+    Route::get('/divisions/{cirle_id}', [GepcoController::class, 'getDivisions']);
+    Route::get('/subdivisions/{division_id}', [GepcoController::class, 'getSubDivisions']);
+    Route::get('/feeders/{sub_division_id}', [GepcoController::class, 'getfeeders']);
+});
